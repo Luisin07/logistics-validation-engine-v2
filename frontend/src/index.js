5db55fd72df874364432a76bf987d4f268a2a82f
Luisin07 <santiniotavio10@gmail.com>
2026-04-14 13:15:24 -0300

    debug: log DATABASE_URL status

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require('cors');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const validateRoute = require('./src/routes/validate');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'definida' : 'UNDEFINED');
+
+app.use('/api/validate', validateRoute);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
